3bccbb418f7138d9fe93dd81d2dcf445af2e8cc1
offfahad <mughalfahad544@gmail.com>
2024-06-25 20:37:19 +0500

    project-complete

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer>
+    <p className='text-center text-lg p-4'>Developed By <a className='underline text-blue-500' href="https://offfahad.vercel.app" target='_blank'>offfahad</a></p>
+   </footer>
+  )
+}
+
+export default Footer
